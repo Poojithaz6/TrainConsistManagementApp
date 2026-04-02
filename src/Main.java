c59f661065ef5d373f64ec461984a06b5fc34667
Poojithaz6 <poojithav183@gmail.com>
2026-04-02 09:53:07 +0530

    UC10: Count Total Seats in Train (reduce)

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,9 @@ class Bogie {
         this.name = name;
         this.capacity = capacity;
     }
-
-    void display() {
-        System.out.println("Bogie: " + name + " | Capacity: " + capacity);
-    }
 }
 
-public class UC9 {
+public class Main{
     public static void main(String[] args) {
 
         List<Bogie> bogies = new ArrayList<>();
@@ -23,21 +19,13 @@ public class UC9 {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 72)); // duplicate type
-        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("Sleeper", 72));
 
-        // Grouping by bogie type (name)
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        // Aggregation using map + reduce
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        // Display grouped result
-        System.out.println("Grouped Bogies by Type:");
-
-        for (String key : groupedBogies.keySet()) {
-            System.out.println("\nType: " + key);
-            for (Bogie b : groupedBogies.get(key)) {
-                b.display();
-            }
-        }
+        System.out.println("Total Seating Capacity: " + totalSeats);
     }
 }
