220e734a7daeb3f64343fd4ac3c403ab3943847f
Poojithaz6 <poojithav183@gmail.com>
2026-04-20 10:36:07 +0530

    UC17: Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,40 +1,25 @@
+import java.util.Arrays;
+
 public class TrainConsistApp {
-
-    // Bubble Sort Method
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    // swap
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    // Utility method to print array
-    public static void display(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
-    }
 
     public static void main(String[] args) {
 
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
 
         System.out.println("Before Sorting:");
-        display(capacities);
+        System.out.println(Arrays.toString(bogieNames));
 
-        bubbleSort(capacities);
+        // Built-in sorting
+        Arrays.sort(bogieNames);
 
         System.out.println("After Sorting:");
-        display(capacities);
+        System.out.println(Arrays.toString(bogieNames));
 
         System.out.println("Program continues normally...");
     }
